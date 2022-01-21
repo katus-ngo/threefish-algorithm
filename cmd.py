89316374bfccadab7b39534f0e29fc9ab6037276
truongnx <truongnxt.1512@gmail.com>
2022-01-21 19:53:15 +0700

    Check file type

--- cmd.py
+++ cmd.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3  
 # -*- coding: UTF-8 -*-
 
-from   util     import magenta, cyan, green, dechunk, rFile, wFile, cls
-from   cts      import MODE_ECB, MODE_CBC
-from   fish     import cipher_threefish_msg
-from   fish     import decipher_threefish_msg
-from   platform import system
+from util import magenta, cyan, green, red, dechunk, rFile, wFile, cls, generate_file_mode
+from cts import MODE_ECB, MODE_CBC
+from fish import cipher_threefish_msg
+from fish import decipher_threefish_msg
+from platform import system
 import keyboard
 import os
 
 # definition of constants
 dictMenu = {
-    "Encrypt" : [
-        { 
-            "ECB" : ["Enter your file path (default assets/message.txt ):"]
+    "Encrypt": [
+        {
+            "ECB": ["Enter your file path (default assets/message.txt ):"]
         },
         {
-            "CBC" : ["Enter your file path (default assets/message.txt ):"]
+            "CBC": ["Enter your file path (default assets/message.txt ):"]
         }
-     ],
-    "Decrypt" : [
-        { 
-            "ECB" : ["Enter your file path (default assets/cipher_text.txt ):"]
+    ],
+    "Decrypt": [
+        {
+            "ECB": ["Enter your file path (default assets/cipher_text.txt ):"]
         },
         {
-            "CBC" : ["Enter your file path (default assets/cipher_text.txt ):"]
+            "CBC": ["Enter your file path (default assets/cipher_text.txt ):"]
         }
     ],
 }
@@ -40,16 +40,16 @@ message_file_path = 'assets/message.txt'
 cipher_file_path = 'assets/cipher_text.txt'
 plain_text_file_path = 'assets/plain_text.txt'
 
-
-M = list(dictMenu)     # F menu
-m = []                 # store menus except the F menu, 
-n = []                 # store idx which has been chosen by user
-idx = 0                # store idx for now, which sign that '=> ' should be at which line
-menu = [dictMenu]      # store complete menu
-rs = []                # rs answers of user
+M = list(dictMenu)  # F menu
+m = []  # store menus except the F menu,
+n = []  # store idx which has been chosen by user
+idx = 0  # store idx for now, which sign that '=> ' should be at which line
+menu = [dictMenu]  # store complete menu
+rs = []  # rs answers of user
 
 L = lambda arr: (arr[len(arr) - 1])
 F = lambda arr: (arr[0])
+
 
 def ask(arr):
     n = len(arr)
@@ -57,8 +57,9 @@ def ask(arr):
         if len(arr) == n:
             rs.append(input('\n-> ' + arr.pop() + '  '))
         else:
-            rs.append(input('-> '   + arr.pop() + '  '))
-    
+            rs.append(input('-> ' + arr.pop() + '  '))
+
+
 # print menu which is m[len(m) - 1] or M
 def show():
     s = S
@@ -75,10 +76,12 @@ def show():
             else:
                 s += green('   ') + cyan(i) + '\n'
     s += E
+    print(s)
+
 
 def init():
     global menu
-    
+
     if len(n) == 1 and len(m) == 0:
         menu.append(L(menu)[M[n[0]]])
         m.append(dechunk([list(i) for i in L(menu)]))
@@ -88,38 +91,47 @@ def init():
             if F(list(i)) == t:
                 menu.append(i[t])
                 m.append(dechunk([list(j) for j in L(menu)]))
-        
+
         if len(n) == 2:
             rs.append(n[1])
             ask([L(L(menu))])
-            
+
             if F(n) == 0:
                 ECB_CBC, s = rs
                 if (not (s and s.strip())):
                     s = message_file_path
-                msg = rFile(s)
+                file_mode = generate_file_mode(s)
+                if len(file_mode) == 0:
+                    print(red('Invalid file format!'))
+                    os._exit(0)
+                msg = rFile(s, file_mode[0])
                 if ECB_CBC == 0:
                     cipher_str = cipher_threefish_msg(msg, MODE_ECB)
-                    wFile(cipher_file_path, cipher_str)
+                    wFile(cipher_file_path, cipher_str, 0, file_mode[1])
                     print(green('Encrypt success!'))
                 elif ECB_CBC == 1:
                     cipher_str = cipher_threefish_msg(msg, MODE_CBC)
-                    wFile(cipher_file_path, cipher_str)
+                    wFile(cipher_file_path, cipher_str, 0, file_mode[1])
                     print(green('Encrypt success!'))
             elif F(n) == 1:
                 ECB_CBC, s = rs
                 if (not (s and s.strip())):
                     s = cipher_file_path
-                msg = rFile(s)
+                file_mode = generate_file_mode(s)
+                if len(file_mode) == 0:
+                    print(red('Invalid file format!'))
+                    os._exit(0)
+                msg = rFile(s, file_mode[0])
                 if ECB_CBC == 0:
                     plain_str = green(decipher_threefish_msg(msg, MODE_ECB))
-                    wFile(plain_text_file_path, plain_str, 5)
+                    wFile(plain_text_file_path, plain_str, 5, file_mode[1])
                     print(green('Decrypt success!'))
                 elif ECB_CBC == 1:
                     plain_str = green(decipher_threefish_msg(msg, MODE_CBC))
-                    wFile(plain_text_file_path, plain_str, 5)
+                    wFile(plain_text_file_path, plain_str, 5, file_mode[1])
                     print(green('Decrypt success!'))
             os._exit(0)
+
 
 # listen left
 def onLeft():
@@ -135,6 +147,7 @@ def onLeft():
     except IndexError:
         show()
 
+
 # listen right
 def onRight():
     global idx
@@ -142,6 +155,7 @@ def onRight():
     idx = 0
     init()
     show()
+
 
 # listen up
 def onUp():
@@ -152,26 +166,27 @@ def onUp():
         idx = idx - 1
     show()
 
+
 # listen down
 def onDown():
     global idx
-    idx =  ((idx + 1) % len(M)) if len(n) == 0 else ((idx + 1) % len(m[len(m) - 1]))
+    idx = ((idx + 1) % len(M)) if len(n) == 0 else ((idx + 1) % len(m[len(m) - 1]))
     show()
+
 
 # print start menu
 show()
 
-
 if system() == 'Windows':
-    keyboard.add_hotkey(72, onUp)     # up clicked
-    keyboard.add_hotkey(80, onDown)   # down clicked
-    keyboard.add_hotkey(75, onLeft)   # left clicked
+    keyboard.add_hotkey(72, onUp)  # up clicked
+    keyboard.add_hotkey(80, onDown)  # down clicked
+    keyboard.add_hotkey(75, onLeft)  # left clicked
     keyboard.add_hotkey(77, onRight)  # right clicked
 else:
-    keyboard.add_hotkey(103, onUp)    # up clicked
+    keyboard.add_hotkey(103, onUp)  # up clicked
     keyboard.add_hotkey(108, onDown)  # down clicked
     keyboard.add_hotkey(105, onLeft)  # left clicked
-    keyboard.add_hotkey(106, onRight) # right clicked
+    keyboard.add_hotkey(106, onRight)  # right clicked
 
 # if 'esc' clicked, exit programme
 keyboard.wait('esc') 
--- util.py
+++ util.py
@@ -1,44 +1,54 @@
 # package for output font colored in cmd
-from   colorama import init
-from   colorama import Fore
+from colorama import init
+from colorama import Fore
+from cts import TEXT_EXTENSION
+
 init(autoreset=True)
 
-#===========================================
-lfsr_mode = [ [1,4], 
-            [1,3],
-            [3,4,5,7],
-            [1,4,7],
-            [2,3,4,5]]
-        
-lfsr_init_str = ['0','1','0','1','0','1','1']
-#===========================================
+# ===========================================
+lfsr_mode = [[1, 4],
+             [1, 3],
+             [3, 4, 5, 7],
+             [1, 4, 7],
+             [2, 3, 4, 5]]
 
-def rotl(x,n):
-    a = x >> n 
-    r = (x - a * 2**n)*(2**(64-n))
+lfsr_init_str = ['0', '1', '0', '1', '0', '1', '1']
+
+
+# ===========================================
+
+def rotl(x, n):
+    a = x >> n
+    r = (x - a * 2 ** n) * (2 ** (64 - n))
     return int(a + r)
+
 
 def rotr(x, n):
     b = padRight(bin(x).replace("0b", ""), "0", 64)
     return int("".join(b[n:] + b[0:n]), 2)
 
+
 def bytesToUtf8(str):
     return str.decode("utf-8", "strict")
 
+
 def padRight(s, char, n):
     return ('{:' + char + '>' + str(n) + '}').format(s)
-     
+
+
 def chunk(arr, n):
     newArr = []
     idx = 0
     length = len(arr)
     while idx < length:
-        newArr.append(arr[idx:idx+n])
+        newArr.append(arr[idx:idx + n])
         idx += n
     return newArr
 
+
 def dechunk(arr):
     return [i for sublist in arr for i in sublist]
+
 
 def clearZero(arr):
     l = len(arr) - 1
@@ -47,30 +57,40 @@ def clearZero(arr):
         l -= 1
     return arr
 
-def rFile(filename, mode = 'r'):
+
+def rFile(filename, mode='r'):
     fo = open(filename, mode)
     str = fo.read()
     fo.close()
     return str
 
-def wFile(filename, str, pos = 0, mode = 'w'):
+
+def wFile(filename, str, pos=0, mode='w'):
     fo = open(filename, mode)
     fo.write(str[pos:])
     return fo.close()
 
 
+def generate_file_mode(filename):
+    l = filename.split('.')
+    last = l[-1]
+    if last in TEXT_EXTENSION:
+        return ['r', 'w']
+    else:
+        return []
+
 
 def readMsg(str, blockSize):
     str_utf8 = str.encode("utf-8")
 
-    # initialisation 
+    # initialisation
     numBlockByte = int(blockSize / 8)
 
     # transfer bytes to arr of byte
     arr = []
     for i in range(len(str_utf8)):
         arr.append(str_utf8[i])
-    
+
     # completion according to blockSize
     length = len(arr)
     if length % numBlockByte != 0:
@@ -89,10 +109,11 @@ def readMsg(str, blockSize):
     # join subArr
     for j in range(len(newArr)):
         newArr[j] = ''.join([padRight(bin(c).replace('0b', ''), '0', 8) for c in newArr[j]])
-    
+
     # return arr of decimal
     numBlock = int(blockSize / 64)
     return chunk([int('0b' + el, 2) for el in newArr], numBlock)
+
 
 def writeMsg(arr):
     newArr = dechunk([chunk(padRight(bin(el).replace('0b', ''), '0', 64), 8) for el in dechunk(arr)])
@@ -103,44 +124,51 @@ def writeMsg(arr):
 
     return bytesToUtf8(bytes(clearZero([int('0b' + el, 2) for el in newArr])))
 
+
 def magenta(str):
     return Fore.MAGENTA + str
+
 
 def green(str):
     return Fore.GREEN + str
 
+
 def cyan(str):
     return Fore.CYAN + str
 
+
 def red(str):
     return Fore.RED + str
+
 
 def cls(arr):
     while len(arr) > 0:
         arr.pop()
 
+
 def lfsr_64bits(lfsr_init_str, l_mode):
     key = lfsr_init_str[0:len(lfsr_init_str)]
     new_bit = lfsr_init_str[l_mode[0]]
 
-    #print(len(lfsr_init_str),len(key))
-    while len(key) < 64 :
-        for i in range(1,len(l_mode)): 
-            new_bit = str(int(new_bit)^int(key[(len(key)-l_mode[i])]))
+    # print(len(lfsr_init_str),len(key))
+    while len(key) < 64:
+        for i in range(1, len(l_mode)):
+            new_bit = str(int(new_bit) ^ int(key[(len(key) - l_mode[i])]))
         key.append(new_bit)
 
-    #transfer str to binary 
-    str_key=''.join(key)
-    
+    # transfer str to binary
+    str_key = ''.join(key)
+
     sum, base = 0, 1
     for i in range(len(str_key)):
-        sum += int(str_key[-i])*base
+        sum += int(str_key[-i]) * base
         base *= 2
-    return sum 
+    return sum
+
 
 def key_generation(nb_key):
-    k = [None]*nb_key
+    k = [None] * nb_key
     for j in range(nb_key):
-        k[j] = lfsr_64bits(lfsr_init_str,lfsr_mode[j])
+        k[j] = lfsr_64bits(lfsr_init_str, lfsr_mode[j])
     # [12434255084567487610, 12582277207756613454, 12440124641901924412, 12469516777473785570]
     return k
